89f6608c25f3bdec4507bee998ed99338234e9ed
CaryLandholt <cary@landholt.com>
2014-05-06 22:12:07 -0500

    chore(cleanup): use tabs no spaces FTW

--- test/test-creation.js
+++ test/test-creation.js
@@ -1,34 +1,35 @@
 /*global describe, beforeEach, it */
-'use strict';
 var path = require('path');
 var helpers = require('yeoman-generator').test;
 
 describe('fatarrow generator', function () {
-  beforeEach(function (done) {
-    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
-      if (err) {
-        return done(err);
-      }
+	beforeEach(function (done) {
+		helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+			if (err) {
+				return done(err);
+			}
 
-      this.app = helpers.createGenerator('fatarrow:app', [
-        '../../app'
-      ]);
-      done();
-    }.bind(this));
-  });
+			this.app = helpers.createGenerator('fatarrow:app', [
+				'../../app'
+			]);
 
-  it('creates expected files', function (done) {
-    var expected = [
-      // add files you expect to exist here.
-    ];
+			done();
+		}.bind(this));
+	});
 
-    helpers.mockPrompt(this.app, {
-      'someOption': true
-    });
-    this.app.options['skip-install'] = true;
-    this.app.run({}, function () {
-      helpers.assertFile(expected);
-      done();
-    });
-  });
+	it('creates expected files', function (done) {
+		var expected = [
+			// add files you expect to exist here.
+		];
+
+		helpers.mockPrompt(this.app, {
+			'someOption': true
+		});
+
+		this.app.options['skip-install'] = true;
+		this.app.run({}, function () {
+			helpers.assertFile(expected);
+			done();
+		});
+	});
 });
