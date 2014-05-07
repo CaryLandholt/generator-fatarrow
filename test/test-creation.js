1608ce3d1371052b66114d222dd9ca1b86a38838
CaryLandholt <cary@landholt.com>
2014-05-06 21:48:35 -0500

    fix(.jshintrc): remove expectation of .jshintrc

--- test/test-creation.js
+++ test/test-creation.js
@@ -20,8 +20,6 @@ describe('fatarrow generator', function () {
   it('creates expected files', function (done) {
     var expected = [
       // add files you expect to exist here.
-      '.jshintrc',
-      '.editorconfig'
     ];
 
     helpers.mockPrompt(this.app, {
