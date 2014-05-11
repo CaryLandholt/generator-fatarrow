2fca88490da997264c48782733404b4b26a80527
CaryLandholt <cary@landholt.com>
2014-05-10 23:17:37 -0500

    test(expectedFiles): do not use

--- test/test-creation.js
+++ test/test-creation.js
@@ -17,19 +17,19 @@ describe('fatarrow generator', function () {
 		}.bind(this));
 	});
 
-	it('creates expected files', function (done) {
-		var expected = [
-			// add files you expect to exist here.
-		];
-
-		helpers.mockPrompt(this.app, {
-			'someOption': true
-		});
-
-		this.app.options['skip-install'] = true;
-		this.app.run({}, function () {
-			helpers.assertFile(expected);
-			done();
-		});
-	});
+	// it('creates expected files', function (done) {
+	// 	var expected = [
+	// 		// add files you expect to exist here.
+	// 	];
+	//
+	// 	helpers.mockPrompt(this.app, {
+	// 		'someOption': true
+	// 	});
+	//
+	// 	this.app.options['skip-install'] = true;
+	// 	this.app.run({}, function () {
+	// 		helpers.assertFile(expected);
+	// 		done();
+	// 	});
+	// });
 });
