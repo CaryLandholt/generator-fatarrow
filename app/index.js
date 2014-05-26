a770b20df39d848543971971479c56bed408ebbb
CaryLandholt <cary@landholt.com>
2014-05-26 12:33:25 -0500

    feat(stats): add plato stats

--- app/index.js
+++ app/index.js
@@ -1,3 +1,5 @@
+// This file is a simple bridge since gulp doesn't support CoffeeScript configurations natively.
+
 require('coffee-script').register();
 
 module.exports = require('./index.coffee');
